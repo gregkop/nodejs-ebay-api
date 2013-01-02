72633c701054c0f9f7643e3d6127560848a1d6a1
jterzi <josephterzi@gmail.com>
2013-01-01 23:29:19 -0500

    fix here

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ var buildXmlInput = function buildXmlInput(opType, params) {
     delete params.authToken;
   }
 
-  if(opType == 'AddItem')
+  if(opType == 'AddItem' || opType == 'ReviseItem')
     top.push(params);
   else
   {
